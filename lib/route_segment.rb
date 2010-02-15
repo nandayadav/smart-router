58ec219df84c7952249593ac4ade349f588cda9d
Nanda Yadav <nyadav@go1on1.com>
2010-02-15 09:05:06 -0700

    everything wrapped in SmartRouter module

--- lib/route_segment.rb
+++ lib/route_segment.rb
@@ -1,20 +1,22 @@
-class RouteSegment
+module SmartRouter
+  class RouteSegment
 
-  attr_reader :path, :model, :options
+    attr_reader :path, :model, :options
 
-  def initialize(path, model=nil, options={})
-    @path = path.to_s
-    @model = model
-    @options = options
+    def initialize(path, model=nil, options={})
+      @path = path.to_s
+      @model = model
+      @options = options
+    end
+
+    def controller
+      value = model.name.underscore if model
+      value.to_s
+    end
+
+    def to_s
+      "path:#{path}, model:#{controller}"
+    end
+
   end
-
-  def controller
-    value = model.name.underscore if model
-    value.to_s
-  end
-
-  def to_s
-    "path:#{path}, model:#{controller}"
-  end
-
 end
---
+++ lib/smart-router.rb
@@ -0,0 +1,5 @@
+module SmartRouter
+end
+require File.join(File.dirname(__FILE__), 'route_segment')
+require File.join(File.dirname(__FILE__), 'route_segment_hash')
+require File.join(File.dirname(__FILE__), 'route_helper')
